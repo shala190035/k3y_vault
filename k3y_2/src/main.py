177e3409aea6783ddd4f9f951b066c7993285db6
shala190035 <55692386+shala190035@users.noreply.github.com>
2024-03-05 11:21:25 +0100

    mhm

--- k3y_2/src/main.py
+++ k3y_2/src/main.py
@@ -8,19 +8,17 @@ import shutil
 # Stellen Sie sicher, dass FastAPI importiert wird, bevor Sie eine Instanz davon erstellen
 app = FastAPI()
 
-# Liste der erlaubten Ursprünge (hier fügen Sie die URL Ihres Frontends ein)
 origins = [
-    "http://localhost:5173",  # Vue-App
-    "http://127.0.0.1:5173"   # Falls Sie auch 127.0.0.1 verwenden
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 
-# CORS Middleware Konfiguration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Alle Ursprünge aus der Liste erlauben
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Erlaube alle Methoden
-    allow_headers=["*"],  # Erlaube alle Header
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 def get_db():
@@ -31,13 +29,11 @@ def get_db():
         db.close()
 
 @app.post("/products/")
-def create_product(title: str, description: str, price: float, image: UploadFile = File(...), db: Session = Depends(get_db)):
-    db_product = Product(title=title, description=description, price=price, image=image.filename)
+def create_product(title: str, description: str, price: float, image: str, db: Session = Depends(get_db)):
+    db_product = Product(title=title, description=description, price=price, image=image)
     db.add(db_product)
     db.commit()
     db.refresh(db_product)
-    with open(f"images/{image.filename}", "wb") as buffer:
-        shutil.copyfileobj(image.file, buffer)
     return db_product
 
 @app.get("/products/")
