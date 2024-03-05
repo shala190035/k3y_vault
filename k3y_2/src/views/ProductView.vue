6c8ac560dbc2968dadb126c0a03d241d1aace59b
shala190035 <55692386+shala190035@users.noreply.github.com>
2024-03-05 11:37:05 +0100

    productpage_design

--- k3y_2/src/views/ProductView.vue
+++ k3y_2/src/views/ProductView.vue
@@ -1,24 +1,33 @@
 <template>
-    <div class="product-view">
+  <div class="product-view">
+    <NavbarComponent />
+    <div class="products-container">
       <ProductComponent />
     </div>
-  </template>
-  
-  <script>
-  import ProductComponent from '@/components/ProductsComponent.vue';
-  
-  export default {
-    components: {
-      ProductComponent,
-    },
-  }
-  </script>
-  
-  <style scoped>
-  .product-view {
-    display: flex;
-    justify-content: center;
-    padding: 20px;
-  }
-  </style>
-  
+    <div class="fixed-bottom">
+    <FooterComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from '../components/NavbarComponent.vue';
+import ProductComponent from '../components/ProductsComponent.vue'; // Achten Sie darauf, den korrekten Pfad anzugeben
+import FooterComponent from '../components/FooterComponent.vue';
+
+export default {
+  components: {
+    NavbarComponent,
+    ProductComponent,
+    FooterComponent,
+  },
+};
+</script>
+
+<style scoped>
+@import "@/css/styles.css";
+
+
+
+/* Sie können hier zusätzliche Stile für ProductView definieren, falls nötig */
+</style>
